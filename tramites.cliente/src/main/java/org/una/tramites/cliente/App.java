18ee80c4e92f687fcd920f730b569a5fa8ea817f
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-09-12 15:54:04 -0600

    loggin

--- tramites.cliente/src/main/java/org/una/tramites/cliente/App.java
+++ tramites.cliente/src/main/java/org/una/tramites/cliente/App.java
@@ -17,19 +17,15 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+        Parent root = FXMLLoader.load(App.class.getResource("login" + ".fxml"));
+        
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
 
     public static void main(String[] args) {
         launch();
