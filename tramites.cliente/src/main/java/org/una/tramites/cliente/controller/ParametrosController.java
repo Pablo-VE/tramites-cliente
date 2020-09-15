d5cb0d90e6598c2b30f0191470460ceeb9819404
Lucho321 <54681195+Lucho321@users.noreply.github.com>
2020-09-15 17:23:08 -0600

    Creación de parametros generales

--- tramites.cliente/src/main/java/org/una/tramites/cliente/controller/ParametrosController.java
+++ tramites.cliente/src/main/java/org/una/tramites/cliente/controller/ParametrosController.java
@@ -5,13 +5,35 @@
  */
 package org.una.tramites.cliente.controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.util.Callback;
+import org.una.tramites.cliente.App;
+import org.una.tramites.cliente.dto.ParametrosGeneralesDTO;
+import org.una.tramites.cliente.service.ParametrosGeneralesService;
+import org.una.tramites.cliente.util.AppContext;
+import org.una.tramites.cliente.util.Mensaje;
+import org.una.tramites.cliente.util.Respuesta;
 
 /**
  * FXML Controller class
@@ -27,18 +49,243 @@ public class ParametrosController implements Initializable {
     @FXML
     private Button btnBorrar;
     @FXML
-    private TableView tbParametros;
-    @FXML
     private Button btnAgregar;
     @FXML
-    private Button btnCerrar;
-
+    private Button botonCerrar;
+    @FXML
+    private ComboBox<String> cbxTipoBusqueda;
+    
+    ParametrosGeneralesDTO registroClick = new ParametrosGeneralesDTO();
+    ParametrosGeneralesService parService = new ParametrosGeneralesService();
+    @FXML
+    private TableView tablaParametros;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        ArrayList<String> opciones = new ArrayList<String>();
+        opciones.add("Por id");
+        opciones.add("Por nombre");
+        opciones.add("Por estado");
+        cbxTipoBusqueda.getItems().clear();
+        cbxTipoBusqueda.getItems().addAll(opciones);
+        cargarPermisosTodos();
+        
     }    
+    public void cargarPermisosTodos(){
+        ArrayList<ParametrosGeneralesDTO> parametrosGeneral = new ArrayList<ParametrosGeneralesDTO>();
+        Respuesta res = parService.getAll();
+        parametrosGeneral=(ArrayList<ParametrosGeneralesDTO>) res.getResultado("ParametrosGeneral");
+        llenarTabla(parametrosGeneral);
+    }
+     public void llenarTabla(ArrayList<ParametrosGeneralesDTO> parametrosGeneral){
+        tablaParametros.getColumns().clear();
+        if(parametrosGeneral!=null){
+            ObservableList items = FXCollections.observableArrayList(parametrosGeneral);   
+
+            TableColumn<ParametrosGeneralesDTO, String> colEstado = new TableColumn("Estado");
+            colEstado.setCellValueFactory(par -> {
+            String estadoString;
+                if(par.getValue().getEstado())
+                    estadoString = "Activo";
+                else
+                    estadoString = "Inactivo";
+                return new ReadOnlyStringWrapper(estadoString);
+                });
+            
+            TableColumn colId = new TableColumn("ID");
+            colId.setCellValueFactory(new PropertyValueFactory("id"));
+            TableColumn colNombre = new TableColumn("Nombre");
+            colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+            TableColumn colValor = new TableColumn("Valor");
+            colValor.setCellValueFactory(new PropertyValueFactory("valor"));
+            TableColumn colDescripcion = new TableColumn("Descripcion");
+            colDescripcion.setCellValueFactory(new PropertyValueFactory("descripcion"));
+            
+            TableColumn colFechaRegistro = new TableColumn("Fecha Registro");
+            colFechaRegistro.setCellValueFactory(new PropertyValueFactory("fechaRegistro"));
+            TableColumn colFechaModificacion = new TableColumn("Fecha Modificacion");
+            colFechaModificacion.setCellValueFactory(new PropertyValueFactory("fechaModificacion"));
+            
+            tablaParametros.getColumns().addAll(colEstado);
+            tablaParametros.getColumns().addAll(colId);
+            tablaParametros.getColumns().addAll(colNombre);
+            tablaParametros.getColumns().addAll(colValor);
+            tablaParametros.getColumns().addAll(colDescripcion);
+            tablaParametros.getColumns().addAll(colFechaRegistro);
+            tablaParametros.getColumns().addAll(colFechaModificacion);
+            addButtonToTable();
+            tablaParametros.setItems(items);
+        }
+    }
+     
+     private void addButtonToTable() {
+        TableColumn<ParametrosGeneralesDTO, Void> colBtn = new TableColumn("Acciones");
+
+        Callback<TableColumn<ParametrosGeneralesDTO, Void>, TableCell<ParametrosGeneralesDTO, Void>> cellFactory = new Callback<TableColumn<ParametrosGeneralesDTO, Void>, TableCell<ParametrosGeneralesDTO, Void>>() {
+            @Override
+            public TableCell<ParametrosGeneralesDTO, Void> call(final TableColumn<ParametrosGeneralesDTO, Void> param) {
+                final TableCell<ParametrosGeneralesDTO, Void> cell = new TableCell<ParametrosGeneralesDTO, Void>() {
+
+                    private final Button btn = new Button("Mod");
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            try{
+                            ParametrosGeneralesDTO data = getTableView().getItems().get(getIndex());
+                            
+                            editarParametrosGenerales(data);
+                            }catch(Exception ex){}
+                        });
+                    }
+                    
+                    private final Button btn2 = new Button("Ver");
+
+                    {
+                        btn2.setOnAction((ActionEvent event) -> {
+                            try{
+                            ParametrosGeneralesDTO data = getTableView().getItems().get(getIndex());
+                            ver(data);
+                            }catch(Exception ex){}
+                        });
+                    }
+                    
+                    HBox pane = new HBox(btn, btn2);
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(pane);
+                            
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        colBtn.setCellFactory(cellFactory);
+
+        tablaParametros.getColumns().add(colBtn);
+
+    }
     
+     public void editarParametrosGenerales(ParametrosGeneralesDTO per) throws IOException{
+        if(per!=null){
+            StackPane Contenedor = (StackPane) AppContext.getInstance().get("Contenedor");
+
+            AppContext.getInstance().set("ModalidadParametros", "Modificar");
+            AppContext.getInstance().set("ParametroEnCuestion", per);
+
+            Parent root = FXMLLoader.load(App.class.getResource("parametrosDetalleInformacion" + ".fxml"));
+            Contenedor.getChildren().clear();
+            Contenedor.getChildren().add(root);
+        }else{
+            Mensaje.showAndWait(Alert.AlertType.WARNING, "Modificar Parametros Generales", "Debes seleccionar un parametro general");
+        }
+    }
+    
+     public void  ver(ParametrosGeneralesDTO per) throws IOException{
+        if(per!=null){
+            StackPane Contenedor = (StackPane) AppContext.getInstance().get("Contenedor");
+
+            AppContext.getInstance().set("ModalidadParametros", "Ver");
+            AppContext.getInstance().set("ParametroEnCuestion", per);
+
+            Parent root = FXMLLoader.load(App.class.getResource("parametrosDetalleInformacion" + ".fxml"));
+            Contenedor.getChildren().clear();
+            Contenedor.getChildren().add(root);
+        }else{
+            Mensaje.showAndWait(Alert.AlertType.WARNING, "Ver Parametros Generales", "Debes seleccionar un parametro general");
+        }
+    } 
+        
+    @FXML
+    private void actBuscar(ActionEvent event) {
+        if(!txtBuscar.getText().isEmpty()){
+            if(tipoBusqueda.equals("id")){
+                cargarParametrosId(Long.valueOf(txtBuscar.getText()));
+            }else{
+                if(tipoBusqueda.equals("estado")){
+                    cargarParametrosEstado(txtBuscar.getText());
+                }else{
+                    if(tipoBusqueda.equals("nombre")){
+                        cargarParametrosNombre(txtBuscar.getText());
+                    }
+                }
+            }
+        }else{
+            Mensaje.showAndWait(Alert.AlertType.WARNING, "Busqueda", "Ingresa el valor a buscar");
+        }
+    }
+
+    private String tipoBusqueda;
+    @FXML
+    private void actTipoBusqueda(ActionEvent event) {
+        txtBuscar.setDisable(false);
+        btnBuscar.setDisable(false);
+        if(cbxTipoBusqueda.getValue().equals("Por id")){
+            tipoBusqueda="id";
+            txtBuscar.setPromptText("Id");
+        }else if(cbxTipoBusqueda.getValue().equals("Por estado")){
+            tipoBusqueda="estado";
+            txtBuscar.setPromptText("Activo o Inactivo");
+        }else{
+            if(cbxTipoBusqueda.getValue().equals("Por nombre")){
+                tipoBusqueda="nombre";
+                txtBuscar.setPromptText("Nombre");
+            }
+        }  
+    }
+    
+     public void cargarParametrosId(Long id){
+        ArrayList<ParametrosGeneralesDTO> parametros = new ArrayList<ParametrosGeneralesDTO>();
+        Respuesta res = parService.getById(id);
+        ParametrosGeneralesDTO per = (ParametrosGeneralesDTO) res.getResultado("ParametrosGeneral");
+        parametros.add(per);
+        llenarTabla(parametros);
+    }
+      public void cargarParametrosEstado(String esta){
+        boolean estado=true;
+        if(esta.equals("activo")||esta.equals("Activo")){
+            estado = true;
+        }else{
+            if(esta.equals("inactivo")||esta.equals("Inactivo")){
+                estado = false;
+            }
+        }
+        ArrayList<ParametrosGeneralesDTO> parametros = new ArrayList<ParametrosGeneralesDTO>();
+        Respuesta res = parService.getByEstado(estado);
+        parametros=(ArrayList<ParametrosGeneralesDTO>) res.getResultado("ParametrosGeneral");
+        llenarTabla(parametros);
+    } 
+    
+      public void cargarParametrosNombre(String nombre){
+        ArrayList<ParametrosGeneralesDTO> parametros = new ArrayList<ParametrosGeneralesDTO>();
+        Respuesta res = parService.getByNombre(nombre);
+        parametros=(ArrayList<ParametrosGeneralesDTO>) res.getResultado("ParametrosGeneral");
+        llenarTabla(parametros);
+    }
+    @FXML
+    private void actBorrar(ActionEvent event) {
+        txtBuscar.setText("");
+        txtBuscar.setPromptText("Consultar por Id, Nombre o Estado");
+        cargarPermisosTodos();
+        Mensaje.showAndWait(Alert.AlertType.INFORMATION, "Parametros Generales", "Parametros disponibles");
+    }
+
+    @FXML
+    private void actAgregar(ActionEvent event) throws IOException {
+        StackPane Contenedor = (StackPane) AppContext.getInstance().get("Contenedor");
+        AppContext.getInstance().set("ModalidadParametros", "Agregar");
+        
+        Parent root = FXMLLoader.load(App.class.getResource("parametrosDetalleInformacion" + ".fxml"));
+        Contenedor.getChildren().clear();
+        Contenedor.getChildren().add(root);
+    }
+
 }
--- tramites.cliente/src/main/java/org/una/tramites/cliente/controller/ParametrosDetalleInformacionController.java
+++ tramites.cliente/src/main/java/org/una/tramites/cliente/controller/ParametrosDetalleInformacionController.java
@@ -6,13 +6,28 @@
 package org.una.tramites.cliente.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import org.una.tramites.cliente.App;
+import org.una.tramites.cliente.dto.ParametrosGeneralesDTO;
+import org.una.tramites.cliente.service.ParametrosGeneralesService;
+import org.una.tramites.cliente.util.AppContext;
+import org.una.tramites.cliente.util.Mensaje;
+import org.una.tramites.cliente.util.Respuesta;
 
 /**
  * FXML Controller class
@@ -26,7 +41,7 @@ public class ParametrosDetalleInformacionController implements Initializable {
     @FXML
     private TextField txtNombre;
     @FXML
-    private ComboBox<?> cmbEstado;
+    private ComboBox<String> cmbEstado;
     @FXML
     private Label lblFechaCreacion;
     @FXML
@@ -36,12 +51,162 @@ public class ParametrosDetalleInformacionController implements Initializable {
     @FXML
     private Button btnGuardar;
 
+    
+    ParametrosGeneralesDTO permiso = new ParametrosGeneralesDTO();
+    private String modalidad;
+    ParametrosGeneralesService perService = new ParametrosGeneralesService();
+    ParametrosGeneralesDTO permisoEditar = new ParametrosGeneralesDTO();
+    @FXML
+    private TextField txtValor;
+    @FXML
+    private TextField txtDescripcion;
+    @FXML
+    private Label LabelId;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        txtId.setDisable(true);
+        Date date = new Date();
+        lblFechaCreacion.setText(date.toString());
+        
+        modalidad = (String) AppContext.getInstance().get("ModalidadParametros");
+        
+        ArrayList<String> estados = new ArrayList<String>();
+        estados.add("Activo");
+        estados.add("Inactivo");
+        cmbEstado.getItems().clear();
+        cmbEstado.getItems().addAll(estados);
+        
+        
+        if(modalidad.equals("Modificar")){
+            permisoEditar = (ParametrosGeneralesDTO) AppContext.getInstance().get("ParametroEnCuestion");
+            txtId.setText(String.valueOf(permisoEditar.getId()));
+            txtNombre.setText(permisoEditar.getNombre());
+            txtValor.setText(permisoEditar.getValor());
+            txtDescripcion.setText(permisoEditar.getDescripcion());
+            if(permisoEditar.getEstado()){
+                cmbEstado.setValue("Activo");
+            }else{
+                cmbEstado.setValue("Inactivo");
+            }
+            lblFechaCreacion.setText(String.valueOf(permisoEditar.getFechaRegistro()));
+            lblFechaModificacion.setText(String.valueOf(permisoEditar.getFechaModificacion()));
+        }else{
+            if(modalidad.equals("Agregar")){
+                txtId.setVisible(false);
+                LabelId.setVisible(false);
+                lblFechaModificacion.setVisible(false);
+                //labelModificacion.setVisible(false);
+            }else{
+                if(modalidad.equals("Ver")){
+                    permisoEditar = (ParametrosGeneralesDTO) AppContext.getInstance().get("ParametroEnCuestion");
+                    txtId.setText(String.valueOf(permisoEditar.getId()));
+                    txtId.setDisable(true);
+                    txtNombre.setText(permisoEditar.getNombre());
+                    txtNombre.setDisable(true);
+                    txtValor.setText(permisoEditar.getValor());
+                    txtValor.setDisable(true);
+                    txtDescripcion.setText(permisoEditar.getDescripcion());
+                    txtDescripcion.setDisable(true);
+                    if(permisoEditar.getEstado()){
+                        cmbEstado.setValue("Activo");
+                    }else{
+                        cmbEstado.setValue("Inactivo");
+                    }
+                    cmbEstado.setDisable(true);
+                    lblFechaCreacion.setText(String.valueOf(permisoEditar.getFechaRegistro()));
+                    lblFechaModificacion.setText(String.valueOf(permisoEditar.getFechaModificacion())); 
+                    btnGuardar.setVisible(false);
+                }
+            }
+        }         
+// TODO
     }    
+
+    @FXML
+    private void actAtras(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("ATRAS");
+        alert.setContentText("¿Esta seguro de que desea regresar a la vista anterior?");
+        Optional<ButtonType> action = alert.showAndWait();
+            if (action.get() == ButtonType.OK) {
+                irPermisos();
+            }
+    }
+
+    public boolean validar(){
+        if(cmbEstado.getValue()==null){
+            return false;
+        }
+        return true;
+    }
     
+    @FXML
+    private void actGuardar(ActionEvent event) {
+         if(validar()){
+            if(modalidad.equals("Agregar")){
+                ParametrosGeneralesDTO nuevoParametro = new ParametrosGeneralesDTO();
+                
+                nuevoParametro.setNombre(txtNombre.getText());
+                nuevoParametro.setValor(txtValor.getText());
+                nuevoParametro.setDescripcion(txtDescripcion.getText());
+                nuevoParametro.setEstado(estado);
+                
+                Respuesta res = perService.guardar(nuevoParametro);
+                if(res.getEstado()){
+                    Mensaje.showAndWait(Alert.AlertType.INFORMATION, "Registro de parametros", "Se agrego el parametro correctamente");
+                    irPermisos();
+                }else{
+                     Mensaje.showAndWait(Alert.AlertType.ERROR, "Registro de parametro", res.getMensaje());
+                }
+            }else{
+                if(modalidad.equals("Modificar")){
+                    permisoEditar.setNombre(txtNombre.getText());
+                    permisoEditar.setValor(txtValor.getText());
+                    permisoEditar.setDescripcion(txtDescripcion.getText());
+                    permisoEditar.setEstado(estado);
+                    Date date= new Date();
+                    permisoEditar.setFechaModificacion(date);
+                    Respuesta res = perService.modificar(permisoEditar.getId(), permisoEditar);
+                    if(res.getEstado()){
+                        Mensaje.showAndWait(Alert.AlertType.INFORMATION, "Edicion de parametro", "Se edito el parametro correctamente");
+                        irPermisos();
+                    }else{
+                         Mensaje.showAndWait(Alert.AlertType.ERROR, "Edicion de parametro", res.getMensaje());
+                    }
+                } 
+            }
+        }else{
+            Mensaje.showAndWait(Alert.AlertType.ERROR, "Informacion de parametro", "Faltan datos por ingresar");
+        }
+    }
+    
+    private boolean estado;
+    @FXML
+    private void actEstado(ActionEvent event) {
+        
+        if(cmbEstado.getValue().equals("Activo")){
+            estado=true;
+        }else if(cmbEstado.getValue().equals("Inactivo")){
+            estado=false;
+        }
+        
+    }
+    
+     public void irPermisos() {
+        try{
+            StackPane Contenedor = (StackPane) AppContext.getInstance().get("Contenedor");
+            Parent root = FXMLLoader.load(App.class.getResource("parametros" + ".fxml"));
+            Contenedor.getChildren().clear();
+            Contenedor.getChildren().add(root);
+        }catch(Exception ex){
+            
+        }
+    }
+
+
 }
